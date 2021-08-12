08aa320236d5c1ee3d010770957f01ac7d1f800e
James Wong <winghojameswong@gmail.com>
2021-08-12 22:21:58 +0800

    Init Spring boot backend

---
+++ instapic/src/main/kotlin/com/jameswong/instapic/InstapicApplication.kt
@@ -0,0 +1,11 @@
+package com.jameswong.instapic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InstapicApplication
+
+fun main(args: Array<String>) {
+	runApplication<InstapicApplication>(*args)
+}
---
+++ instapic/src/test/kotlin/com/jameswong/instapic/InstapicApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jameswong.instapic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InstapicApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

f532ccff7003ce42adabf6555957591b30b3fa94
James Wong <winghojameswong@gmail.com>
2021-08-13 13:18:43 +0800

    Block user registration

--- instapic/src/main/kotlin/com/jameswong/instapic/user/UserService.kt
+++ instapic/src/main/kotlin/com/jameswong/instapic/user/UserService.kt
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Service
 @Service
 class UserService(val userRepository: UserRepository, val passwordEncoder: PasswordEncoder) {
     fun register(username: String, password: String) {
+        val existingUser = userRepository.findUserByUsername(username);
+        if (existingUser != null) {
+            throw Exception("Username in use") // TODO: handling
+        }
         userRepository.save(User(username, passwordEncoder.encode(password)))
     }
 }

d95e67685dd67789b8cb870cb200755d8c83d5ac
James Wong <winghojameswong@gmail.com>
2021-08-13 09:55:29 +0800

    enable h2-console for debug

--- instapic/src/main/kotlin/com/jameswong/instapic/SecurityConfig.kt
+++ instapic/src/main/kotlin/com/jameswong/instapic/SecurityConfig.kt
@@ -1,18 +1,25 @@
 package com.jameswong.instapic
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig: WebSecurityConfigurerAdapter() {
+    // TODO: Block all other then /register
     override fun configure(http: HttpSecurity) {
         http.authorizeRequests()
             .anyRequest()
             .permitAll()
             .and()
             .csrf().disable()
+            .headers().frameOptions().disable() // TODO: Remove
     }
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
 }

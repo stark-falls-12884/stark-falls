386491591cd7f9f15950f5abef2400d21fa6309b
James Wong <winghojameswong@gmail.com>
2021-08-14 14:31:27 +0800

    ImageService use FileService as backend

--- instapic/src/main/kotlin/com/jameswong/instapic/image/ImageService.kt
+++ instapic/src/main/kotlin/com/jameswong/instapic/image/ImageService.kt
@@ -1,11 +1,20 @@
 package com.jameswong.instapic.image
 
+import com.jameswong.instapic.file.FileService
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 
 @Service
-class ImageService {
+class ImageService(val fileService: FileService) {
     fun saveImage(image: MultipartFile): String {
-        return "dummy.png"
+        return fileService.saveFile(image)
+    }
+
+    fun getImageUrl(id: String): String {
+        return fileService.getFilePath(id)
+    }
+
+    fun delete(id: String) {
+        fileService.deleteFile(id)
     }
 }

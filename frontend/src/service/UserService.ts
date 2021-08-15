4eb85952981509ccf81b8567b5fe107f9c086445
James Wong <winghojameswong@gmail.com>
2021-08-15 18:58:24 +0800

    [frontend] Add react-router-dom types, react-hok-form

---
+++ frontend/src/service/UserService.ts
@@ -0,0 +1,20 @@
+import wretch from "wretch";
+
+interface LoginRequest {
+  username: string;
+  password: string;
+}
+
+export class UserService {
+  static login(request: LoginRequest): Promise<string> {
+    return wretch("/api/user/login").post(request).text();
+  }
+
+  static register(request: LoginRequest): Promise<void> {
+    return wretch("/api/user/register").post(request);
+  }
+
+  static logout(): Promise<void> {
+    return wretch("api/user/logout").post();
+  }
+}

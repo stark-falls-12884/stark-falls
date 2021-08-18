88ec4e4efb8558fe2d73425a7844ad5ec4608a88
James Wong <winghojameswong@gmail.com>
2021-08-18 20:58:20 +0800

    [frontend] Add PostService

---
+++ frontend/src/service/PostService.ts
@@ -0,0 +1,45 @@
+import wretch from "wretch";
+
+export interface GetPostsRequest {
+  pageNumber: number;
+  pageSize: number;
+}
+
+export interface NewPostRequest {
+  body: string;
+  image: File; // TODO: Proper type
+}
+
+export interface Post {
+  body: string;
+  createdTie: string;
+  id: number;
+  authorName: string;
+  imageUrl: string;
+}
+
+export interface GetPostResponse {
+  totalPages: number;
+  totalElements: number;
+  number: number;
+  size: number;
+  numberOfElements: number;
+  content: Post[];
+}
+
+export class PostService {
+  static getPosts(request: GetPostsRequest): Promise<GetPostResponse> {
+    return wretch("/api/post").query(request).get().json();
+  }
+
+  static getPostsByAuthor(author: string, request: GetPostsRequest): Promise<GetPostResponse> {
+    return wretch(`/api/post/${author}`).query(request).get().json();
+  }
+
+  static newPost(request: NewPostRequest) {
+    const formData = new FormData();
+    formData.set("body", request.body);
+    formData.set("image", request.image);
+    return wretch(`/api/post`).formData(formData).post();
+  }
+}
